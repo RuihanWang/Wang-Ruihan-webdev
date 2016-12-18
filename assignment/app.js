44d4fd161d73b79721363099b5dc25f70d340481
RuihanWang <wangruihan01@gmail.com>
2016-12-18 09:17:12 -0500

    a

--- assignment/app.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-    var model = require("./model/model.server")();
-    require("./services/user.service.server.js")(app,model);
-    require("./services/website.service.server.js")(app,model);
-    require("./services/page.service.server.js")(app,model);
-    require("./services/widget.service.server.js")(app,model);
-
-
-};
